c5f217ef3b704dd954c05923d45a0aed4f15d775
Bohdan Marko <goldenbohdanm@gmail.com>
2023-11-24 22:31:37 +0200

    Create models for train_station app

--- train_station/models.py
+++ train_station/models.py
@@ -1,3 +1,194 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="source_routes",
+    )
+    destination = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="destination_routes",
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source}-{self.destination}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Facility(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Facilities"
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains",
+    )
+    facility = models.ManyToManyField(
+        Facility,
+        related_name="trains",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.name} (Type: {self.train_type})"
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Crew(models.Model):
+    POSITION_CHOICES = [
+        ('driver', 'Train Driver'),
+        ('attendant', 'Train Attendant'),
+        ('engineer', 'Train Engineer'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.CharField(
+        max_length=25,
+        choices=POSITION_CHOICES,
+        default="attendant"
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} | {self.position}"
+
+    class Meta:
+        ordering = ["last_name"]
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="journeys"
+    )
+    train = models.ForeignKey(
+        Train,
+        on_delete=models.CASCADE,
+        related_name="journeys"
+    )
+    crew = models.ManyToManyField(
+        Crew,
+        related_name="trains"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.route} (Departure: {self.departure_time})"
+
+    class Meta:
+        ordering = ["-departure_time"]
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        Journey,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self):
+        return f"{self.journey} | Cargo & Seat : {self.cargo} & {self.seat}"
+
+    @staticmethod
+    def validate_ticket(cargo, seat, train, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, train_attr_name in [
+            (cargo, "cargo", "cargo_num"),
+            (seat, "seat", "places_in_cargo")
+        ]:
+            count_attrs = getattr(train, train_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {train_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.cargo, self.seat, self.journey.train, ValidationError
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    class Meta:
+        unique_together = (
+            "cargo",
+            "seat",
+            "journey",
+        )
+        ordering = ["cargo", "seat"]
